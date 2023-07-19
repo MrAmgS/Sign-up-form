21a895d5c2e9ab54c82009e7b07bc822d9ceb3d9
AMHGH <aprogrammer2023@gmail.com>
2023-07-19 18:40:00 +0330

    form fields and base functions of SignUp  created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import {Route , Routes , Navigate} from 'react-router-dom';
 
 //Components
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,11 +1,95 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    return (
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+  const [touched, setTouched] = useState({});
+
+  const changeHandler = (event) => {
+    if (event.target.name === "isAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+  };
+
+  const focusHandler = (event) => {
+    setTouched({ ...touched, [event.target.name]: true });
+  };
+
+  return (
+    <div>
+      <form>
+        <h1>Sign Up</h1>
         <div>
-            
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            value={data.name}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
         </div>
-    );
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter a password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+        </div>
+        <div>
+          <label>Conform Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password again"
+            name="confirmPassword"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+        </div>
+        <div>
+          <div>
+            <label>I accept terms of privacy policy</label>
+            <input
+              type="checkbox"
+              name="isAccepted"
+              value={data.isAccepted}
+              onChange={changeHandler}
+              onFocus={focusHandler}
+            />
+          </div>
+        </div>
+        <div>
+            <Link to='/login' >Log in</Link>
+            <button type="submit" >Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
