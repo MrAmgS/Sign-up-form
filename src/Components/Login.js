7dc809b4f746bf9240a7e13d624479df643f8a38
MrAmgS <a.h.gharaei@hotmail.com>
2023-07-20 18:59:14 +0330

    Log In form completed and  toastify added to it

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+//helper functions
+import validate from "./validate";
+
+//Toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { notify } from "./toast";
+
+//styles
+import styles from "./SignUp.module.css";
 
 const Login = () => {
-    return (
-        <div>
-            
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+
+  useEffect(() => {
+    setErrors(validate(data, "login"));
+  }, [data, touched]);
+
+  const changeHandler = (event) => {
+      setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const focusHandler = (event) => {
+    setTouched({ ...touched, [event.target.name]: true });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!Object.keys(errors).length) {
+      notify("Loged In Successfully", "success");
+    } else {
+      notify("Invalid Username or Wrong Password", "warning");
+      setTouched({
+        email: true,
+        password: true,
+      });
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.formContainer} onSubmit={submitHandler}>
+        <h1 className={styles.header}>Log In</h1>
+        <div className={styles.formField}>
+          <label>Email</label>
+          <input
+            className={
+              errors.email && touched.email
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            type="text"
+            placeholder="Enter your email"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.email && touched.email && <span>{errors.email}</span>}
         </div>
-    );
+        <div className={styles.formField}>
+          <label>Password</label>
+          <input
+            className={
+              errors.password && touched.password
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            type="password"
+            placeholder="Enter a password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.password && touched.password && (
+            <span>{errors.password}</span>
+          )}
+        </div>
+        
+        <div className={styles.formButtons}>
+          <Link to="/signup">Sign Up</Link>
+          <button type="submit">Log In</button>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/toast.js
+++ src/Components/toast.js
@@ -6,5 +6,7 @@ export const notify = (text , type) => {
         toast.success(text)
     } else if (type === "error"){
         toast.error(text)
+    } else if (type === "warning"){
+        toast.warning(text)
     }
 } ;
